329d379384cb640454078ff4f7f3d667ef6d75d5
Tiago Pereira <tiagomrp06@gmail.com>
2020-05-02 20:47:54 +0100

    corrects minor bugs

--- DynWebao/Emitter.cs
+++ DynWebao/Emitter.cs
@@ -15,7 +15,7 @@ namespace DynWebao
                 MethodAttributes.Public |
                 MethodAttributes.SpecialName |
                 MethodAttributes.RTSpecialName,
-                CallingConventions.Standard,
+                CallingConventions.HasThis,
                 new[] {typeof(IRequest)});
 
             ILGenerator ilGen = constructorBuilder.GetILGenerator();
@@ -31,23 +31,20 @@ namespace DynWebao
                                                                                     MethodAttributes.ReuseSlot,
                 methodInfo.ReturnType, methodInfo.Args.ToArray());
 
+            
             ILGenerator ilGen = methodBuilder.GetILGenerator();
             ilGen.DeclareLocal(typeof(string));
             ilGen.Emit(OpCodes.Ldstr, methodInfo.Get.path);
             ilGen.Emit(OpCodes.Stloc_0);
             ilGen.Emit(OpCodes.Ldarg_0);
-            /* ldfld class [Webao]Webao.IRequest DynWebao.ArtistWebaoDummy::req
-             * TODO: LDFLD do req - >
-             */ 
-            ilGen.Emit(OpCodes.Ldfld, typeof(IRequest));
-            
+            ilGen.Emit(OpCodes.Ldfld, typeof(Base).GetField("req"));
             ilGen.Emit(OpCodes.Ldloc_0);
             ilGen.Emit(OpCodes.Ldc_I4, methodInfo.Args.Count);
             ilGen.Emit(OpCodes.Newarr, typeof(string));
             ilGen.Emit(OpCodes.Dup);
-            ilGen.Emit(OpCodes.Ldc_I4_0);
-            for (int i = 0; i < methodInfo.Args.Count; i++)
+            for (var i = 0; i < methodInfo.Args.Count; i++)
             {
+                ilGen.Emit(OpCodes.Ldc_I4,i);
                 ilGen.Emit(OpCodes.Ldarg, i + 1);
                 ilGen.Emit(OpCodes.Callvirt, typeof(object).GetMethod("ToString",Type.EmptyTypes));
                 ilGen.Emit(OpCodes.Stelem_Ref);
@@ -56,8 +53,9 @@ namespace DynWebao
             } 
             ilGen.Emit(OpCodes.Call, typeof(Base).GetRuntimeMethods().ElementAt(0));
             ilGen.Emit(OpCodes.Ldtoken, methodInfo.Mapping.destType);
-            ilGen.Emit(OpCodes.Callvirt, typeof(Type).GetMethod("GetTypeFromHandle",
+            ilGen.Emit(OpCodes.Call, typeof(Type).GetMethod("GetTypeFromHandle",
                 BindingFlags.Public | BindingFlags.Static) ?? throw new Exception());
+            ilGen.Emit(OpCodes.Callvirt, typeof(IRequest).GetMethod("Get"));
             ilGen.Emit(OpCodes.Castclass, methodInfo.Mapping.destType);
             GenResult(ilGen, methodInfo.Mapping);
             ilGen.Emit(OpCodes.Ret);
