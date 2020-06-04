e4632b632aaf45d21a5918e818f43439b7986896
Tiago Pereira <tiagomrp06@gmail.com>
2020-06-04 13:49:29 +0100

    starts phase 3 development

---
+++ GenDynWebao/ServiceTracks.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Webao;
+using Webao.Test.Dto.LastFm;
+using Webao.Test.LastFmWebaos;
+
+namespace GenDynWebao
+{
+    public class ServiceTracks
+    {
+        private readonly WebaoTrack webao;
+        public ServiceTracks() : this(new HttpRequest()) {}
+
+        private ServiceTracks(IRequest req)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Track> TopTracksFrom(string country)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
